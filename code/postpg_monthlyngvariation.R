608f29eea9c7198fee63cf6f4bf75c5800b8ade8
Qingyu Xu <qingyux@princeton.edu>
2022-06-25 16:47:01 -0400

    NG Price Variation: add function of having naturalgaspriceadder
    use `ngpriceadder`

--- code/postpg_monthlyngvariation.R
+++ code/postpg_monthlyngvariation.R
@@ -1,11 +1,20 @@
-
 ngmultiplier = read_csv('./data/Natural_Price_Factor_2019_NAfixed.csv', col_types = cols()) %>%
   pivot_longer(cols = -c(Month), names_to = 'fuel', values_to = 'multiplier')
 fuel_price <- read_csv(paste0(RunFdr,'/Fuels_data.csv'), col_types = cols())[-1,]
-fuel_rowone <- read_csv(paste0(RunFdr,'/Fuels_data.csv'), col_types = cols())[1,] # this row store the emission factor.
+fuel_price_original <- read_csv(paste0(RunFdr,'/Fuels_data.csv'), col_types = cols())
+if (grepl("superhighngcost",RunFdr)) {
+  for (c in 1:ncol(fuel_price)) {
+    if (grepl("naturalgas", colnames(fuel_price)[c])) {
+      fuel_price[,c] = fuel_price[,c] + ngpriceadder
+    }
+  }
+}
+# fuel_rowone stores the emission factor.
+fuel_rowone <- read_csv(paste0(RunFdr,'/Fuels_data.csv'), col_types = cols())[1,] 
 fuel_order = colnames(fuel_price)[-1]
 reprepoint <- read_csv(paste0(RunFdr,'/Representative_Period.csv'), col_types = cols())
-rep_hour <- select(read_csv(paste0(RunFdr,'/Load_data.csv'), col_types = cols()), Timesteps_per_Rep_Period)$Timesteps_per_Rep_Period[1]
+rep_hour <- select(read_csv(paste0(RunFdr,'/Load_data.csv'), col_types = cols()), 
+                   Timesteps_per_Rep_Period)$Timesteps_per_Rep_Period[1]
 
 mapping = tibble(
   Period = paste('p',ceiling(c(1:8760)/rep_hour),sep=""),
@@ -16,7 +25,8 @@ fuel_price_variate <- fuel_price %>%
   mutate(Period = rep(reprepoint$slot, each = rep_hour),
          Period_Hour = rep(c(1:rep_hour), length = rep_hour*(dim(reprepoint)[1]))) %>%
   left_join(mapping) %>%
-  pivot_longer(cols = -c(Time_Index, Period, Period_Hour, Month), names_to = 'fuel', values_to = 'price') %>%
+  pivot_longer(cols = -c(Time_Index, Period, Period_Hour, Month), 
+               names_to = 'fuel', values_to = 'price') %>%
   left_join(ngmultiplier)
 fuel_price_variate$multiplier[which(is.na(fuel_price_variate$multiplier))] <- 1
 fuel_price_variate <- fuel_price_variate %>%
@@ -25,6 +35,6 @@ fuel_price_variate <- fuel_price_variate %>%
   select(-c(Period, Period_Hour, Month, price, multiplier)) %>%
   pivot_wider(id_cols = Time_Index, names_from = 'fuel', values_from = 'price_var')
 write_csv(rbind(fuel_rowone, fuel_price_variate), paste0(RunFdr,'/Fuels_data.csv'))
-write_csv(fuel_price, paste0(RunFdr,'/Fuels_data_original.csv'))
+write_csv(fuel_price_original, paste0(RunFdr,'/Fuels_data_original.csv'))
 
 rm(fuel_price_variate, fuel_price, mapping, rep_hour, reprepoint, fuel_order, fuel_rowone, ngmultiplier)
