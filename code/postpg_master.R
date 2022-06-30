b63a27a8aa002af0716093e0cd7eb350c081274f
Qingyu Xu <qingyux@della8.princeton.edu>
2022-06-30 14:43:15 -0400

    update master file for co2 tax results

--- code/postpg_master.R
+++ code/postpg_master.R
@@ -4,7 +4,7 @@ library('yaml')
 
 # workingfolder <- "/Users/qingyuxu/Dropbox (Princeton)/PJM/GenX_PJM_Data/2022_PJM_CE/"
 workingfolder <- "/tigress/qingyux/GenX/PJM/2022_PJM_CE/"
-foldername_fn <- "2022-06-25_names_createnewfolder.csv"
+foldername_fn <- "2022-06-30_names_createnewfolder.csv"
 miscfoldername <- "PJM_misc_files/"
 files_to_copy_from_misc <- c('CO2_credit.csv',
                             'Capacity_reserve_margin_slack.csv',
@@ -14,7 +14,7 @@ name_of_shell = 'Run_myopic.sh'
 name_of_runfile = 'Run_myopic_ce.jl'
 years = c("2025","2030")
 n_totalhour = 4368
-tofolder = 'pjm_ce_0627/'
+tofolder = 'pjm_ce_0630/'
 frompgfolder = 'pjm_ce/'
 fromfolder = 'pjm_ce_modified'
 ngprice_variation = 'census_division' # chose from 'national' and 'census_division'
