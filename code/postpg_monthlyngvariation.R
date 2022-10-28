fcf75bafd5c70a6de4048d67a169ce3913af3e83
Qingyu Xu <qingyux@princeton.edu>
2022-10-28 19:32:10 -0400

    modify the code so it works with the full time-series

--- code/postpg_monthlyngvariation.R
+++ code/postpg_monthlyngvariation.R
@@ -12,12 +12,20 @@ if (grepl("superhighngcost",RunFdr)) {
 # fuel_rowone stores the emission factor.
 fuel_rowone <- read_csv(paste0(RunFdr,'/Fuels_data.csv'), col_types = cols())[1,] 
 fuel_order = colnames(fuel_price)[-1]
-reprepoint <- read_csv(paste0(RunFdr,'/Representative_Period.csv'), col_types = cols())
-rep_hour <- select(read_csv(paste0(RunFdr,'/Load_data.csv'), col_types = cols()), 
-                   Timesteps_per_Rep_Period)$Timesteps_per_Rep_Period[1]
+reprepoint_fn <- paste0(RunFdr,'/Representative_Period.csv')
+if (file.exists(reprepoint_fn)) {
+  reprepoint <- read_csv(paste0(RunFdr,'/Representative_Period.csv'), 
+                         col_types = cols())
+  rep_hour <- select(read_csv(paste0(RunFdr,'/Load_data.csv'), 
+                              col_types = cols()), 
+                     Timesteps_per_Rep_Period)$Timesteps_per_Rep_Period[1]
+} else {
+  reprepoint = as_tibble_col("p1", column_name = "slot")
+  rep_hour = 8760
+}
 
 mapping = tibble(
-  Period = paste('p',ceiling(c(1:8760)/rep_hour),sep=""),
+  Period = paste('p',ceiling(c(1:8760)/rep_hour), sep=""),
   Period_Hour = rep(c(1:rep_hour), length = 8760),
   Month = rep(c(1:12), times = c(31,28,31,30,31,30,31,31,30,31,30,31)*24))
 
